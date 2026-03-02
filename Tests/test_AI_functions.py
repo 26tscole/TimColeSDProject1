38afa0c10744d8876773765098c7f0d47c54866c
26tscole <123657398+26tscole@users.noreply.github.com>
2026-03-02 01:08:39 -0500

    just learned about pytest fixtures it was a lifesaver. hopefully this fixes the problem in github because it works fine in pycharm

--- Tests/test_AI_functions.py
+++ Tests/test_AI_functions.py
@@ -1,23 +1,27 @@
 from pathlib import Path
+import pytest
 from src.AIconnection.agent import runAIChat
 from src.meetIngActions.users import Users
-
 
 project_root = Path(__file__).parent
 
 
-def test_runAIChat():
+@pytest.fixture
+def test_user_booking():
     roomId = 11
     startTime = "2040-01-01 10:00 AM"
     endTime = "2040-01-01 10:15 AM"
     testUser = Users()
     bookId = testUser.bookMeeting(roomId=roomId, startTime=startTime, endTime=endTime)
-    print(bookId)
+    yield
+    testUser.deleteBooking(bookId=bookId)
+
+
+def test_runAIChat(test_user_booking):
     audioFilePath = str(project_root) + "/TestingFiles/AIAudioTest.wav"
     response = runAIChat(source="file", audioFilePath=audioFilePath)
     assert response
-    foundStartTime = response[0]['data'][0]['start_time']
-    foundEndTime = response[0]['data'][0]['end_time']
-    assert foundStartTime == '2040-01-01T10:00:00Z'
-    assert foundEndTime == '2040-01-01T10:15:00Z'
-    testUser.deleteBooking(bookId=bookId)
+    foundStartTime = response[0]["data"][0]["start_time"]
+    foundEndTime = response[0]["data"][0]["end_time"]
+    assert foundStartTime == "2040-01-01T10:00:00Z"
+    assert foundEndTime == "2040-01-01T10:15:00Z"
