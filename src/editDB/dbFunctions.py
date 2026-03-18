e21e4ba353cb2671c877fb632898e0b8c7ff91a2
26tscole <123657398+26tscole@users.noreply.github.com>
2026-03-18 15:31:53 -0400

    DB functions all work just have to implement them into the GUI and make tests for them. Currently trying to figure out how I will change the servers database using my functions.

--- src/editDB/dbFunctions.py
+++ src/editDB/dbFunctions.py
@@ -1,4 +1,40 @@
 import sqlite3
+from pathlib import Path
 
-def addRoom():
-    conn = sqlite3.connect('db.sqlite3')
+def DBConnection():
+    DBPath = Path(__file__).resolve().parent.parent.parent / "db.sqlite3"
+    return sqlite3.connect(DBPath)
+
+def getAllRooms():
+    with DBConnection() as conn:
+        query = "SELECT id, room_name, capacity FROM booking_meetingroom WHERE is_active"
+        return conn.execute(query).fetchall()
+
+
+def addRoom(roomName, Capacity):
+    with DBConnection() as conn:
+        cur = conn.cursor()
+        query = "INSERT INTO booking_meetingroom (room_name, capacity, is_active) VALUES (?, ?, 1)"
+        cur.execute(query, (roomName, Capacity))
+        conn.commit()
+        return cur.fetchall()
+
+def deleteRoom(roomId):
+    with DBConnection() as conn:
+        bookedRoomsQuery="SELECT * FROM booking_bookinghistory WHERE meeting_room_id=?"
+        bookedRooms = conn.execute(bookedRoomsQuery, (roomId,)).fetchall()
+        deleteBookedRooms = "DELETE FROM booking_bookinghistory WHERE id = ?"
+        conn.execute(deleteBookedRooms, (roomId,))
+        deleteRoomQuery="DELETE FROM booking_meetingroom WHERE id = ?"
+        conn.execute(deleteRoomQuery, (roomId,))
+        conn.commit()
+        return bookedRooms
+
+def updateCapacity(roomId, capacity):
+    with DBConnection() as conn:
+        cur = conn.cursor()
+        query = "UPDATE booking_meetingroom SET capacity = ? WHERE id = ?"
+        cur.execute(query, (capacity, roomId))
+        conn.commit()
+        return cur.rowcount
+
